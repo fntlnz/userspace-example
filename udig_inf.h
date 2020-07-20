3e4d411ef1e24c47f102473114041d5344f475bc
Lorenzo Fontana <fontanalorenz@gmail.com>
2020-07-20 18:01:01 +0200

    initial commit
    Signed-off-by: Lorenzo Fontana <fontanalorenz@gmail.com>

---
+++ udig_inf.h
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+
+#include "userspace_types.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include "ppm_events.h"
+#include "ppm_events_public.h"
+
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+struct event_data_t {
+  enum ppm_capture_category category;
+  bool compat;
+
+  union {
+    struct {
+      uint64_t *regs;
+      long id;
+      const enum ppm_syscall_code *cur_g_syscall_code_routing_table;
+    } syscall_data;
+  } event_info;
+};
+
+size_t strlcpy(char *dst, const char *src, size_t size);
---
+++ userspace.cc
@@ -0,0 +1,24 @@
+
+#include "userspace_types.h"
+
+#include "ppm_ringbuffer.h"
+#include "scap.h"
+#include <sys/user.h>
+
+static struct udig_ring_buffer_status *g_ring_status = NULL;
+static struct ppm_ring_buffer_info *g_ring_info = NULL;
+static uint8_t *g_ring = NULL;
+int g_ring_fd = -1;
+uint32_t g_ringsize = 0;
+char g_console_print_buf[256];
+int g_ring_descs_fd = -1;
+static char g_str_storage[PAGE_SIZE];
+
+int main(int argc, char const *argv[]) {
+
+  int res;
+  res = udig_alloc_ring(&g_ring_fd, &g_ring, &g_ringsize, g_console_print_buf);
+  if (res < 0) {
+    return res;
+  }
+}
